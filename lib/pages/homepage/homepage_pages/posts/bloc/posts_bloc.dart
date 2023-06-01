39c8bd50b6394e4fd8e85eff3b95335ef243459e
Priyanshu078 <priyanshupaliwal078@gmail.com>
2023-06-01 23:19:30 +0530

    cleaned some code

--- lib/pages/homepage/homepage_pages/posts/bloc/posts_bloc.dart
+++ lib/pages/homepage/homepage_pages/posts/bloc/posts_bloc.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:instagram_clone/data/posts_data.dart';
 part 'posts_event.dart';
 part 'posts_state.dart';
 
