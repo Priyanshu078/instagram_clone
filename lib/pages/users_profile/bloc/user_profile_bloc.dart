1afe941e45f7db882374eb3606920ccce0a42663
Priyanshu078 <priyanshupaliwal078@gmail.com>
2023-06-07 21:58:00 +0530

    removed unnecessary files

--- lib/pages/users_profile/bloc/user_profile_bloc.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:equatable/equatable.dart';
-
-part 'user_profile_event.dart';
-part 'user_profile_state.dart';
-
-class UserProfileBloc extends Bloc<UserProfileEvent, UserProfileState> {
-  UserProfileBloc() : super(UserProfileInitial()) {}
-}
--- lib/pages/users_profile/bloc/user_profile_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'user_profile_bloc.dart';
-
-abstract class UserProfileEvent extends Equatable {
-  const UserProfileEvent();
-
-  @override
-  List<Object> get props => [];
-}
